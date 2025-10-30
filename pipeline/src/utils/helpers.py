f3a3acab91d97b94496ee8d8fda6be425138fd21
Cesar <2213110208@untels.edu.pe>
2025-10-30 17:38:13 -0500

    creando utils

---
+++ pipeline/src/utils/helpers.py
@@ -0,0 +1,37 @@
+class Helpers:
+    @staticmethod
+    def validate_user_input(username, password):
+        """
+        Valida la entrada de usuario para el login (por implementar)
+        Args:
+            username: Nombre de usuario
+            password: Contraseña
+        Returns:
+            bool: True si los datos son válidos, False en caso contrario
+        """
+        # TODO: Implementar validación de entrada de usuario
+        pass
+    
+    @staticmethod
+    def format_currency(amount):
+        """
+        Formatea un monto como moneda (por implementar)
+        Args:
+            amount: Cantidad a formatear
+        Returns:
+            str: Monto formateado como moneda
+        """
+        # TODO: Implementar formateo de moneda
+        pass
+    
+    @staticmethod
+    def validate_product_data(product_data):
+        """
+        Valida los datos de un producto (por implementar)
+        Args:
+            product_data: Datos del producto a validar
+        Returns:
+            bool: True si los datos son válidos, False en caso contrario
+        """
+        # TODO: Implementar validación de datos de producto
+        pass
